1e68bbd7c70019141b549ae6af1e78c12e942ce8
ChiaCaruso <carusochiara08@gmail.com>
2022-02-08 16:30:21 +0100

    feature: realizzazione del footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,11 +1,23 @@
-import styles from "./Footer.module.scss"
+import { FaFacebookF } from 'react-icons/fa';
+import { FiInstagram } from 'react-icons/fi';
+import { AiOutlineTwitter } from 'react-icons/ai';
+
+import styles from "./Footer.module.scss";
 
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <p>&copy; 'Ddocu</p>
+            <div className={styles.footerText}>
+                 <p>&copy; 'Ddocu</p>
+                <p>ddocu@gmail.com</p>
+            </div>
+            <div className={styles.footerSocial}>
+                <FaFacebookF className={styles.social_icon} />
+                <FiInstagram className={styles.social_icon} />
+                <AiOutlineTwitter className={styles.social_icon} />
+            </div>
         </footer>
-    )
-}
+    );
+};
 
 export default Footer;
