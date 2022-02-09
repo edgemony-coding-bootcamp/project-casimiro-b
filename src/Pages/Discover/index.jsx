c1c79ae3771a6224faabea3b5c7a7ba16b358fa3
Giuseppe Gallo Cassarino <giuseppegallocassarino@MBP-di-Giuseppe.home-life.hub>
2022-02-09 16:40:13 +0100

    feat:modichecard

--- src/Pages/Discover/index.jsx
+++ src/Pages/Discover/index.jsx
@@ -1,33 +1,9 @@
-import DiscoverCard from './DiscoverCard';
-import { useState, useEffect } from 'react';
-import '../../Tools/discover.json';
-
-const data = require('../../Tools/discover.json');
-
 const Discover = () => {
 
-    const [card, setCard] = useState();
-
-    useEffect(() => {
-        setCard(data);
-        console.log(data);
-    }, [])
 
 
     return (
-        <div>
-            {card?.map((item, index) =>
-                <DiscoverCard
-                    key={index}
-                    title={item.title}
-                    description={item.description}
-                    price={item.price}
-                    icon={item.icon}
-                    image={item.image}
-                    date={item.date}
-                />)}
-
-        </div>
+        <></>
 
     )
 }
