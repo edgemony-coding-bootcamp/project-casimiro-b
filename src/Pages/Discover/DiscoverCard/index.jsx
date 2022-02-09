05292f9fc29de27ad83af1e9db6e8a25b2cb59a4
Andrea <andrea.aguiducci@gmail.com>
2022-02-09 12:22:56 +0100

    add: partial discover card

---
+++ src/Pages/Discover/DiscoverCard/index.jsx
@@ -0,0 +1,22 @@
+import styles from './DiscoverCard.module.scss';
+
+const DiscoverCard = ({ date, title, icon, description, price, image }) => {
+    return (
+        <div className={styles.discoverCard}>
+            <div className={styles.title}>
+                <h2>{date}</h2>
+                <h4>{title}</h4>
+                <img src={icon} alt='prova' />
+            </div>
+            <div className={styles.description}>
+                <img src={image} alt='prova' />
+                <p>{description}</p>
+                <div className={styles.price}>
+                    <p>{price}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DiscoverCard;
