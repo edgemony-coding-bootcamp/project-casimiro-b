688f95fb4f315c2458c79b4bf9ea8458eb69daec
Margherita Grasso <margherita.grasso3@gmail.com>
2022-02-09 20:10:57 +0100

    fix: homepage responsive

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,47 +1,23 @@
-import { Parallax } from "react-parallax";
+import ParallaxComponent from "./ParallaxComponent";
 import styles from "./Home.module.scss";
 
 const Home = () => {
   const images = [
     "https://cdn.discordapp.com/attachments/940161700341116933/940533480423563264/photo-1563693267403-111c5d856e49.png",
-    "https://images.unsplash.com/photo-1543872668-29e25522ac77?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-    "https://images.unsplash.com/photo-1528917715128-68f319d04c0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80",
+    "https://images.unsplash.com/photo-1559424092-88b61cb7a6c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    "https://images.unsplash.com/photo-1514936477380-5ea603b9a1ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
   ];
 
   return (
+  
     <div className={styles.parallaxDiv}>
-      <Parallax bgImage={images[0]} strength={500} className={styles.customBg}>
-        <div style={{ height: "100vh" }}>
-          <div className={styles.textParallax}>
-            <h2>Lorem ipsum ponosm.</h2>
-            <p>
-              ‘Ddocu è ipsum ponosm entiv. Pregt aferade. <br/>
-              Folingar bast, biosam,lorem ipsum ponosm entiv.
-            </p>
-          </div>
-        </div>
-      </Parallax>
-      <Parallax bgImage={images[1]} strength={500} className={styles.customBg}>
-        <div style={{ height: "100vh" }}></div>
-        <div className={styles.textParallax}>
-            <h2>Lorem ipsum ponosm.</h2>
-            <p>
-              ‘Ddocu è ipsum ponosm entiv. Pregt aferade. <br/>
-              Folingar bast, biosam,lorem ipsum ponosm entiv.
-            </p>
-          </div>
-      </Parallax>
-      <Parallax bgImage={images[2]} strength={500} className={styles.customBg}>
-        <div style={{ height: "100vh" }}></div>
-        <div className={styles.textParallax}>
-            <h2>Lorem ipsum ponosm.</h2>
-            <p>
-              ‘Ddocu è ipsum ponosm entiv. Pregt aferade. <br/>
-              Folingar bast, biosam,lorem ipsum ponosm entiv.
-            </p>
-          </div>
-      </Parallax>
+      {images.map((item, index) => (
+
+          <ParallaxComponent image={item} key={index} />
+
+      ))}
     </div>
+
   );
 };
 
