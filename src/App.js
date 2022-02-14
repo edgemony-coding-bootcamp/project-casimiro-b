52dd886beb85cb45ea3cbf9b3f8487b930205bfb
giulia notaro <notarogiulia10@gmail.com>
2022-02-14 12:29:57 +0100

    feat:insert white logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import { Suspense, lazy } from 'react';
-
+import { Suspense, lazy } from "react";
+import logo from "./Ddocubianco.svg";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Loading from "./Components/Loading";
@@ -10,51 +10,57 @@ const Discover = lazy(() => import("./Pages/Discover"));
 const Experience = lazy(() => import("./Pages/Experience"));
 const AboutUs = lazy(() => import("./Pages/AboutUs"));
 
-
 const INIT_STATE = {
-  name: "'Ddocu",
+  name: <img src={logo} />,
   nav: [
     { link: "/", label: "HOME" },
     { link: "/AboutUs", label: "ABOUT US" },
-
     { link: "/Discover", label: "DISCOVER" },
     { link: "/Experience", label: "EXPERIENCE" },
   ],
 };
 
-
 function App() {
   return (
     <>
       <Header name={INIT_STATE.name} links={INIT_STATE.nav} />
-      
+
       <Routes>
-
-        <Route path="/" element={
-          <Suspense fallback={<Loading />}>
-            <Home />
-          </Suspense>
-        } />
-        <Route path="/AboutUs" element={
-          <Suspense fallback={<Loading />}>
-            <AboutUs />
-          </Suspense>
-        } />
-        <Route path="/Discover" element={
-          <Suspense fallback={<Loading />}>
-            <Discover />
-          </Suspense>
-        } />
-        <Route path="/Experience" element={
-          <Suspense fallback={<Loading />}>
-            <Experience />
-          </Suspense>
-        } />
-
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<Loading />}>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/AboutUs"
+          element={
+            <Suspense fallback={<Loading />}>
+              <AboutUs />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/Discover"
+          element={
+            <Suspense fallback={<Loading />}>
+              <Discover />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/Experience"
+          element={
+            <Suspense fallback={<Loading />}>
+              <Experience />
+            </Suspense>
+          }
+        />
       </Routes>
 
       <Footer />
-
     </>
   );
 }
