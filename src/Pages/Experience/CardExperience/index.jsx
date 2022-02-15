4c5719e08f6e03e1720d6ec01c0e733cee762627
ChiaCaruso <carusochiara08@gmail.com>
2022-02-15 19:30:34 +0100

    feat: implementata la logica nella pagina experience

--- src/Pages/Experience/CardExperience/index.jsx
+++ src/Pages/Experience/CardExperience/index.jsx
@@ -1,14 +1,14 @@
 import styles from "./CardExperience.module.scss";
 
-const CardExperience = () => {
+const CardExperience = ({ image, title, description }) => {
     return (
         <div className={styles.card}>
-            
+
             <div className={styles.description}>
-                <img src={"https://images.unsplash.com/photo-1508144753681-9986d4df99b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" }></img>
-                <h4>Lorem Ipsum</h4>
-                <p>Parliamo di Palazzo Alliata di Pietratagliata, una delle più significative dimore quattrocentesche della città, che apre le porte per ben 4 visite esclusive da febbraio a maggio, in naturale continuità con il RestArt, il festival che la scorsa estate ha aperto in notturna alcuni dei siti culturali più belli e più importanti della città.</p>
-                
+                <img src={image}></img>
+                <h4>{title}</h4>
+                <p>{description}</p>
+
             </div>
         </div>
     )
