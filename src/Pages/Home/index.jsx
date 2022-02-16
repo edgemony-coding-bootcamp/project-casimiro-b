f7bbde87016aff413b79e63741960eaaf34be260
Margherita Grasso <margherita.grasso3@gmail.com>
2022-02-16 20:17:52 +0100

    feat: scrolling behavior homepage

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -6,6 +6,21 @@ import Carousel from "react-elastic-carousel";
 import { useSelector, useDispatch } from "react-redux";
 import { FETCH_ALL_DATA_CARD } from "../../store/action";
 
+import {
+  Animator,
+  ScrollContainer,
+  ScrollPage,
+  batch,
+  Fade,
+  FadeIn,
+  Move,
+  MoveIn,
+  MoveOut,
+  Sticky,
+  StickyIn,
+  ZoomIn,
+} from "react-scroll-motion";
+
 import styles from "./Home.module.scss";
 import { Link } from "react-router-dom";
 
@@ -25,9 +40,8 @@ const Home = () => {
 
   const description = [
     `‘Ddocu è ipsum ponosm entiv. Pregt aferade. Folingar bast, biosam,lorem ipsum ponosm entiv. ‘Ddocu è ipsum ponosm entiv. Pregt aferade. Folingar bast, biosam,lorem ipsum ponosm entiv.`,
-    `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error esse eius quod incidunt consequuntur omnis quae ab pariatur. Adipisci cumque neque iste dignissimos dolore velit assumenda, iure quaerat eius nemo?`
+    `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error esse eius quod incidunt consequuntur omnis quae ab pariatur. Adipisci cumque neque iste dignissimos dolore velit assumenda, iure quaerat eius nemo?`,
   ];
-
 
   const breakPoints = [
     { width: 1, itemsToShow: 1 },
@@ -36,9 +50,69 @@ const Home = () => {
     { width: 1200, itemsToShow: 4 },
   ];
 
+  const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+  const FadeUp = batch(Fade(), Sticky(), Move());
+
   return (
     <>
-      <div className={styles.parallaxDiv}>
+      <ScrollContainer>
+        <ScrollPage page={0}>
+          <div className={styles.bgScroll}>
+            <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -200))}>
+              <h2>noi siamo 'Ddocu.</h2>
+            </Animator>
+          </div>
+        </ScrollPage>
+        <ScrollPage page={1}>
+          <div className={styles.bgScroll}>
+            <Animator animation={ZoomInScrollOut}>
+              <h2>e vi stupiremooooo</h2>
+            </Animator>
+          </div>
+        </ScrollPage>
+        <ScrollPage page={2}>
+          <div className={styles.section_3}>
+            <h2>
+              <Animator animation={MoveIn(-1000, 0)}>PERCHE'</Animator>
+              <Animator animation={MoveIn(1000, 0)}>SIAMO</Animator>
+              <Animator animation={MoveOut(1000, 0)}>BRAVI</Animator>
+              <Animator animation={MoveOut(-1000, 0)}>BRAVISSIMI</Animator>
+            </h2>
+          </div>
+        </ScrollPage>
+        <ScrollPage page={3}>
+          <div className={styles.bgScroll}>
+            <Animator animation={FadeUp}>
+              <h2>ma tantooooooo</h2>
+            </Animator>
+          </div>
+        </ScrollPage>
+        <ScrollPage page={4}>
+          <div className={styles.bgScroll}>
+            <Animator animation={batch(Fade(), Sticky())}>
+              <h1>SCEGLI LA TUA META.</h1>
+            </Animator>
+          </div>
+        </ScrollPage>
+      </ScrollContainer>
+
+      <div className={styles.carouselDiv}>
+        <div className={styles.containercity}>
+          <Carousel breakPoints={breakPoints}>
+            {card?.map((item, index) => (
+              <Link key={index} to={`/${item.id}`}>
+                <CardHomepage
+                  key={index}
+                  title={item.title}
+                  descriptioncity={item.description}
+                  imagecity={item.imagecity}
+                />
+              </Link>
+            ))}
+          </Carousel>
+        </div>
+      </div>
+      {/* <div className={styles.parallaxDiv}>
         <ParallaxComponent
           className={styles.parallaxOne}
           image={images[0]}
@@ -46,28 +120,7 @@ const Home = () => {
         />
         <ParallaxComponentCenter  description={description[1]} />
         <ParallaxComponent  image={images[1]} description={description[0]}/>
-      </div>
-
-
-    <div className={styles.carouselDiv}>
-
-      <h2 className={styles.titleContainer}>SCEGLI LA TUA META</h2>
-
-      <div className={styles.containercity}>
-        <Carousel breakPoints={breakPoints}>
-          {card?.map((item, index) => (
-            <Link key={index} to={`/${item.id}`}>
-              <CardHomepage
-                key={index}
-                title={item.title}
-                descriptioncity={item.description}
-                imagecity={item.imagecity}
-              />
-            </Link>
-          ))}
-        </Carousel>
-      </div>
-    </div>
+      </div> */}
     </>
   );
 };
