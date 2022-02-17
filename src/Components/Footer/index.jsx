cb19f06225f52c83f193aa298c2a49ed4d31bd5d
Andrea <andrea.aguiducci@gmail.com>
2022-02-17 16:53:34 +0100

    fix: date footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -5,6 +5,8 @@ import { AiOutlineTwitter } from 'react-icons/ai';
 import styles from "./Footer.module.scss";
 
 const Footer = () => {
+    const year = new Date().getFullYear();
+
     return (
         <footer className={styles.footer}>
             <div className={styles.footerSocial}>
@@ -13,14 +15,14 @@ const Footer = () => {
                 <AiOutlineTwitter className={styles.social_icon} />
             </div>
             <div className={styles.footerText}>
-                 <p>&copy; Copyright 2022 ' Ddocu</p>
-                 <p>All Rights Reserved – gampStudio </p>
-                 <h4>hi@ddocu.com</h4>
+                <p>&copy; Copyright {year} ' Ddocu</p>
+                <p>All Rights Reserved – gampStudio </p>
+                <h4>hi@ddocu.com</h4>
             </div>
             <div className={styles.footerContact}>
-            <p>Contattaci</p>
-            <p>Iscriviti</p>
-            <p>Servizio Clienti</p>
+                <p>Contattaci</p>
+                <p>Iscriviti</p>
+                <p>Servizio Clienti</p>
             </div>
         </footer>
     );
