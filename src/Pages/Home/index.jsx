3684deaffbb8dcadccb191e7ffbeb02b2595d519
giulia notaro <notarogiulia10@gmail.com>
2022-02-17 12:56:39 +0100

    css in progress

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -59,61 +59,89 @@ const Home = () => {
         <ScrollPage page={0}>
           <div className={styles.bgScroll}>
             <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -200))}>
-              <h2>noi siamo 'Ddocu.</h2>
+              <h2>37°35′21″N / 14°08′53″E</h2>
             </Animator>
           </div>
         </ScrollPage>
         <ScrollPage page={1}>
           <div className={styles.bgScroll}>
             <Animator animation={ZoomInScrollOut}>
-              <h2>e vi stupiremooooo</h2>
+              <h3>è l'odore della terra</h3>
             </Animator>
           </div>
         </ScrollPage>
         <ScrollPage page={2}>
-          <div className={styles.section_3}>
-            <h2>
-              <Animator animation={MoveIn(-1000, 0)}>PERCHE'</Animator>
-              <Animator animation={MoveIn(1000, 0)}>SIAMO</Animator>
-              <Animator animation={MoveOut(1000, 0)}>BRAVI</Animator>
-              <Animator animation={MoveOut(-1000, 0)}>BRAVISSIMI</Animator>
-            </h2>
-          </div>
+            <Animator animation={batch(Fade(), Sticky())}>
+
+
+              <img src="https://images.unsplash.com/photo-1605447781678-2a5baca0e07b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"/>
+
+            </Animator>
         </ScrollPage>
         <ScrollPage page={3}>
           <div className={styles.bgScroll}>
             <Animator animation={FadeUp}>
-              <h2>ma tantooooooo</h2>
+              <h3>è il vento che sa di mare</h3>
             </Animator>
           </div>
         </ScrollPage>
         <ScrollPage page={4}>
+            <Animator animation={batch(Fade(), Sticky())}>
+              <img src="https://images.unsplash.com/photo-1601324353981-5f920af43146?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80"/>
+            </Animator>
+        </ScrollPage>
+        <ScrollPage page={5}>
+            <Animator animation={batch(Fade(), Sticky())}>
+              <img src="https://images.unsplash.com/photo-1617103099853-6dd647eb1489?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"/>
+            </Animator>
+            <Animator animation={FadeUp}>
+            <h2 className={styles.sky}>è il cielo che non finisce mai.</h2>
+            </Animator>
+        </ScrollPage>
+        {/* <ScrollPage page={2}>
+          <div className={styles.section_3}>
+            <h2>
+              <Animator animation={MoveIn(-1000, 0)}>land</Animator>
+              <Animator animation={MoveIn(1000, 0)}>water</Animator>
+              <Animator animation={MoveOut(1000, 0)}>air</Animator>
+              <Animator animation={MoveOut(-1000, 0)}>fire</Animator>
+            </h2>
+          </div>
+        </ScrollPage> */}
+
+        {/* <ScrollPage page={6}>
           <div className={styles.bgScroll}>
             <Animator animation={batch(Fade(), Sticky())}>
-              <h1>SCEGLI LA TUA META.</h1>
+              <h3>è il cielo che non finisce mai.</h3>
+            </Animator>
+          </div>
+        </ScrollPage> */}
+
+
+        <ScrollPage page={6}>
+          <div className={styles.bgScrollMin}>
+            <Animator animation={batch(Fade(), Sticky())}>
+              <h2>SCEGLI LA TUA META.</h2>
             </Animator>
           </div>
         </ScrollPage>
       </ScrollContainer>
 
-
-    <div className={styles.carouselDiv}>
-
-      <div className={styles.containercity}>
-        <Carousel breakPoints={breakPoints}>
-          {card?.map((item, index) => (
-            <Link key={index} to={`/${item.id}`}>
-              <CardHomepage
-                key={index}
-                title={item.title}
-                imagecity={item.imagecity}
-              />
-            </Link>
-          ))}
-        </Carousel>
+      <div className={styles.carouselDiv}>
+        <div className={styles.containercity}>
+          <Carousel breakPoints={breakPoints}>
+            {card?.map((item, index) => (
+              <Link key={index} to={`/${item.id}`}>
+                <CardHomepage
+                  key={index}
+                  title={item.title}
+                  imagecity={item.imagecity}
+                />
+              </Link>
+            ))}
+          </Carousel>
+        </div>
       </div>
-    </div>
-
     </>
   );
 };
