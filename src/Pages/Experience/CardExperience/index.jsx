a526bb412be740e62bbf55105827991a6a2a5fca
Margherita Grasso <margherita.grasso3@gmail.com>
2022-02-18 12:49:32 +0100

    fix: error css modal and card experience

--- src/Pages/Experience/CardExperience/index.jsx
+++ src/Pages/Experience/CardExperience/index.jsx
@@ -1,13 +1,21 @@
 import styles from "./CardExperience.module.scss";
 
 const CardExperience = ({ handleOpen, event }) => {
+
+   
     return (
         <div className={styles.card} onClick={() => handleOpen(event.uuid)}>
 
             <div className={styles.description}>
-                <img src={event.cover_image_url}></img>
+                <div className={styles.img_border} style={{backgroundImage:`url(${event.cover_image_url})`}}>
+                {/* <img src={event.cover_image_url}></img> */}
+
+                </div>
+                <div>
                 <h3>{event.title}</h3>
                 <p>{event.description}</p>
+
+                </div>
 
             </div>
         </div>
