434100ded826f85cdab52cc031561b45d074653e
Andrea <andrea.aguiducci@gmail.com>
2022-02-18 15:27:12 +0100

    prove

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,13 +1,16 @@
 import styles from "./LogIn.module.scss";
 import { createUserWithEmailAndPassword, onAuthStateChanged, signOut, signInWithEmailAndPassword } from 'firebase/auth';
 import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from "react-redux";
+import { USER_SIGNUP } from "../../store/action";
 import { auth } from '../../firebase-config';
 import Log from "../../Components/Log";
 
 
 const SignIn = () => {
 
-
+  const dispatch = useDispatch();
+  const userSigned = useSelector((state) => state.user)
   const imageLogIn = {
     log_in: { backgroundImage: 'url("https://images.unsplash.com/photo-1601833384276-f8b2d26b1170?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80")' },
     sign_in: { backgroundImage: 'url("https://images.unsplash.com/photo-1631733156581-2e204995e8d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80")' }
@@ -16,9 +19,8 @@ const SignIn = () => {
 
   const [registerEmail, setRegisterEmail] = useState('');
   const [registerPassword, setRegisterPassword] = useState('');
-  const [loginEmail, setLoginEmail] = useState('');
-  const [loginPassword, setLoginPassword] = useState('');
   const [user, setUser] = useState({});
+
   console.log(auth)
   onAuthStateChanged(auth, (currentUser) => {
     setUser(currentUser);
@@ -37,19 +39,9 @@ const SignIn = () => {
     }
 
   }
-  const logIn = async () => {
-    try {
-      const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
-      console.log(user)
-    }
-    catch (err) {
-      console.log(err)
-      console.log('Email o password non valide');
-    }
-  }
-  const logOut = async () => {
-    await signOut(auth);
-  }
+  useEffect(() => {
+    dispatch(USER_SIGNUP(user))
+  }, [user])
 
   return (
     <div className={styles.logIn_wrapper}>
