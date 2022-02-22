211b1133855f8a3f3aa695a6224e30c80d7a05df
giulia notaro <notarogiulia10@gmail.com>
2022-02-22 12:43:14 +0100

    add:homevideo

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -14,10 +14,15 @@ import {
   MoveOut,
   Sticky,
   StickyIn,
+  Zoom,
+  FadeIn,
+  MoveIn,
 } from "react-scroll-motion";
 
 import styles from "./Home.module.scss";
 import { Link } from "react-router-dom";
+import video from "./video.mp4";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 import { icons } from "react-icons";
 
 const Home = () => {
@@ -32,9 +37,7 @@ const Home = () => {
   const breakPoints = [
     { width: 1, itemsToShow: 1 },
     { width: 550, itemsToShow: 2 },
-    { width: 970, itemsToShow: 3 },
-    { width: 1200, itemsToShow: 4 },
-    { width: 1600, itemsToShow: 5 },
+ 
   ];
 
   const ZoomInScrollOut = batch(StickyIn(), Fade());
@@ -43,92 +46,64 @@ const Home = () => {
   return (
     <>
       <ScrollContainer>
+
         <ScrollPage page={0}>
           <div className={styles.bgScroll}>
-
-            <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -200))}>
-
+            <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -300))}>
               <div className={styles.firstScroll}>
-               
-
-              <div className={styles.secondText}>
-
+                <div className={styles.secondText}>
                   <h1>'Ddocu</h1>
-                  
-              
-                 <button>Conosci l'isola.</button>
+
+                  <AnchorLink href="#carousel">
+                    <button>Conosci l'isola.</button>
+                  </AnchorLink>
 
                   <h3>
                     Scopri gli eventi previsti in città, <br></br>
                     prenota l'esperienza che ti ispira di più <br></br> e scopri
                     l'isola insieme a noi.
                   </h3>
-                
-
-                
-              </div>
-
-              
-              
-
-
-
+                  
+                </div>
 
                 <div className={styles.elements}>
                   <h4>Inizia il viaggio.</h4>
                   <CgScrollV className={styles.iconScroll} />
                 </div>
-
-               
               </div>
-
-
             </Animator>
           </div>
         </ScrollPage>
-        
+
+     
+
         <ScrollPage page={1}>
-          <div className={styles.bgScroll}>
-            <Animator animation={FadeUp}>
-              <h3>C'è la terra nei miei ricordi,</h3>
-            </Animator>
-          </div>
-        </ScrollPage>
-        <ScrollPage page={2}>
-          <Animator animation={batch(Fade(), Sticky())}>
-            <img src="https://images.unsplash.com/photo-1538074261162-c0b1526af15a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80" />
-          </Animator>
-        </ScrollPage>
-        <ScrollPage page={3}>
-          <div className={styles.bgScroll}>
-            <Animator animation={FadeUp}>
-              <h3>c'è il vento che sa di mare</h3>
-            </Animator>
-          </div>
-        </ScrollPage>
 
-        <ScrollPage page={4}>
-          <Animator animation={batch(Fade(), Sticky())}>
-            <img src="https://images.unsplash.com/photo-1584892177250-542d04a9afe5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80" />
-          </Animator>
-        </ScrollPage>
+         <video
+          src={video}
+          loading="lazy"
+          autoPlay
+          muted
+          loop
+          playsinline
+        ></video>
 
-        <ScrollPage page={5}>
+
           <div className={styles.bgScroll}>
-            <Animator animation={FadeUp}>
+
+            <Animator animation= {batch(Fade(0, 1,), Sticky(50, 50, 0), Move())}>
+              <h3>C'è la terra nei miei ricordi, <br></br>
+              c'è il vento che sa di mare <br></br>
               <h3>ed un cielo che non finisce mai.</h3>
+              </h3>
             </Animator>
+          
+           
           </div>
         </ScrollPage>
 
-        <ScrollPage page={6}>
-          <Animator animation={batch(Fade(), Sticky())}>
-            <img src="https://images.unsplash.com/photo-1586729150659-40c989036a30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80" />
-          </Animator>
-        </ScrollPage>
-
-        <ScrollPage page={7}>
-          <div className={styles.carouselDiv}>
+        <ScrollPage page={2}>
+          <div id="carousel" className={styles.carouselDiv}>
             <div className={styles.leftText}>
               <h1>
                 Scegli la tua meta, <br></br> scegli il tuo ricordo.
