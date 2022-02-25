b650a1290c54759bfbebb48cea3d69e9fd2f985f
Andrea <andrea.aguiducci@gmail.com>
2022-02-25 09:53:01 +0100

    fix ?

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
   return (
     <>
       {/* dash={Object.keys(user).length === 0 ? '/Log' : '/dashboard'} logOut={LogOut} user={user} */}
-      <Header name={INIT_STATE.name} links={INIT_STATE.nav} dash={user.length === 0 ? '/Log' : '/dashboard'} logOut={LogOut} user={user} />
+      <Header name={INIT_STATE.name} links={INIT_STATE.nav} dash={user?.length === 0 ? '/Log' : '/dashboard'} logOut={LogOut} user={user} />
 
 
 
